33dc223004efe23e67c74e34c2b84f7f786ad867
Kevin Klein <kklein166@gmail.com>
2022-10-15 18:06:42 +0000

    my lo-down

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ module.exports.each = each;
  module.exports.identity = identity;
 
 
+
  /**
   * typeOf: Function takes in an input value and returns data type of input value
   * 
@@ -60,7 +61,68 @@ module.exports.each = each;
  module.exports.typeOf = typeOf; 
 
 
+
  /**
-  * 
-  * 
+  * first: Function takes in an array and a number as inputs and return the first number of items in array
+  * @param {Array}: Function takes in an input array
+  * @param {Number}: Function takes in an input number
+  * @return {Array or Number}: Function returns an array literal if input array is not an array, the first element in input array if input number is not a number or not given, or any array of the first number of items in input array
   */
+
+  _.first = function(arr, num) { 
+    if(Array.isArray(arr) === false || num < 0) { 
+        return [];
+    }
+    if(isNaN(num) === true) { 
+        return arr[0]; 
+    }
+    if(num > arr.length) { 
+        return arr; 
+    }
+    return arr.splice(0, num); 
+}
+
+module.exports.first = first;
+
+
+
+ /**
+  * last: Function takes in an array and a number as inputs and return the last number of items in array
+  * @param {Array}: Function takes in an input array
+  * @param {Number}: Function takes in an input number
+  * @return {Array or Number}: Function returns an array literal if input array is not an array, the last element in input array if input number is not a number or not given, or any array of the last number of items in input array
+  */
+
+  _.last = function(arr, num) { 
+    if(Array.isArray(arr) === false || num < 0) { 
+        return []; 
+    }  
+    if(isNaN(num) === true) { 
+        return arr[arr.length - 1]; 
+    }
+    if(num > arr.length) { 
+        return arr; 
+    }
+    return arr.slice(-num); 
+}
+
+module.exports.last = last; 
+
+
+/**
+ * indexOf: Function takes in an array and a value as inputs and either returns the index of array that is the first occurance of value or returns -1 if value is not in array
+ * @param {Array}: Function takes in an input array
+ * @param {Value}: Function takes in an input value
+ * @return {Number}: Function returns the index of input array that is the first occurance of value or -1 if value is not in array
+ */
+
+ function indexOf(arr, val) { 
+    for(var i = 0; i < arr.length; i++) {  
+        if(val === arr[i]) {  
+            return i; 
+        }
+    }
+    return -1; 
+}
+
+module.exports.indexOf = indexOf; 
